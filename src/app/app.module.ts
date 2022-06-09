deecc5a5dcc2e6160b488778c9884cc47c0e2a0f
ochoapjavier <ochoapjavier@gmail.com>
2022-06-09 23:02:24 +0200

    Cambios en dashboard sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,9 +19,16 @@ import { ContactoComponent } from './contacto/contacto.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { FormUsuarioComponent } from './usuario/form-usuario.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { PistaTenisComponent } from './pista/pista-tenis.component';
-import { FormPistaTenisComponent } from './pista/form-pista-tenis.component';
+import { PistaTenisComponent } from './pista-tenis/pista-tenis.component';
+import { FormPistaTenisComponent } from './pista-tenis/form-pista-tenis.component';
 import { SuperficieComponent } from './superficie/superficie.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 const routes:Routes=[
   {path:'', component:IndexComponent},
@@ -56,14 +64,22 @@ const routes:Routes=[
       DashboardComponent,
       PistaTenisComponent,
       FormPistaTenisComponent,
-      SuperficieComponent,
+      SuperficieComponent
+      
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule, 
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servicio-pistas.service.ts
+++ src/app/servicio-pistas.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { PistaTenis } from './pista/pista-tenis';
+import { PistaTenis } from './pista-tenis/pista-tenis';
 
 @Injectable({
   providedIn: 'root'
